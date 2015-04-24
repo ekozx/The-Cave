d0708a8fd86f97db3049255dd36732a4a4911033
Evan Kozliner <evankozliner@gmail.com>
2015-04-24 11:07:48 -0400

    fixed up the form a little. Starting to implement groups

--- models/group.js
+++ models/group.js
@@ -1,9 +1,6 @@
 var mongoose = require('mongoose');
 
 var postSchema = mongoose.Schema({
-  picture: String,
-  title: String,
-  body: String,
   created_at: {type: Date, default: Date.now}
 });
 
