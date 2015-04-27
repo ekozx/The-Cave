894e3e5d63a48d4f8723469322f8b43b43424c2d
Evan Kozliner <evankozliner@gmail.com>
2015-04-27 10:50:05 -0400

    adding some small erro r checking

--- public/javascripts/limbo/dashboardUpload.js
+++ public/javascripts/limbo/dashboardUpload.js
@@ -1,19 +1,38 @@
 $(function() {
-  $('#file_input').on( "change", function() {
-    alert("hola");
-    file = $('#file_input').val();
-    console.log(file);
+  $('#file_input').change(function() {
+    var file = this.files[0];
+    name = file.name;
+    size = file.size;
+    type = file.type;
+
     if(file == null) {
-      console.log("no files");
+      alert("No files detected.");
     } else {
-      console.log(file);
-      get_signed_request(file);
+      if(file_test(file)) {
+        getSignedRequest(file);
+      }
     }
   });
 });
+function file_test(file) {
+  result = true;
 
-function get_signed_request(file) {
-  url = "/sign_s3?file_name=" + file.name + "&file_type=" + file.type;
+  if(file.name.length < 1) {
+    result = false;
+    alert("File needs a name.");
+  } else if(file.type != 'image/png' && 
+            file.type != 'image/jpg' && 
+            file.type != 'image/gif' && 
+            file.type != 'image/jpeg' ) {
+    result = false;
+    alert("File is not an acceptable type. Please use png, jpg, gif or jpeg extensions.");
+  }
+
+  return result;
+}
+function getSignedRequest(file) {
+  type = file.split(".")[1];
+  url = "/sign_s3?file_name=" + file + "&file_type=" + type;
   $.get(url, function(data) {
     console.log(data);
   });
